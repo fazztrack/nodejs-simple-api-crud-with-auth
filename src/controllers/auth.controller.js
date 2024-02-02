f7adc6124885ac3d00cf2e867eba26f43ca66059
fazztrack <campers0915@gmail.com>
2024-02-02 16:58:13 +0700

    fix: fix http status code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -10,26 +10,26 @@ module.exports = {
       const result = await authModel.login(email)
       const user = result[0]
       if(!user){
-        res.json({
+        res.status(401).json({
           message: "login failed, email or password wrong"
         })
         return
       }
       const passwordVerified = await argon2.verify(user.password, password)
       if(!passwordVerified){
-        res.json({
+        res.status(401).json({
           message: "login failed, email or password wrong"
         })
         return
       }
       const token = jwt.sign({email: user.email}, JWT_SECRET);
-      res.json({
+      res.status(200).json({
         token,
         message: "login success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
   register: async(req, res) => {
@@ -37,13 +37,13 @@ module.exports = {
       const {email, password} = req.body
       const hash = await argon2.hash(password);
       const result = await authModel.register({email, password: hash})
-      res.json({
+      res.status(200).json({
         data: result,
         message: "register success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   }
 }
--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -3,39 +3,39 @@ module.exports = {
   getAll: async(req, res) => {
     try {
       const result = await bookModel.getAll()
-      res.json({
+      res.status(200).json({
         data: result,
         message: "get all books success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
   getDetail: async(req, res) => {
     try {
       const id = req.params.id
       const result = await bookModel.getDetail(id)
-      res.json({
+      res.status(200).json({
         data: result[0],
         message: "get detail book success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
   create: async(req, res) => {
     try {
       const {title, description, author} = req.body
       const result = await bookModel.create({title, description, author})
-      res.json({
+      res.status(200).json({
         data: result,
         message: "create book success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
   update: async(req, res) => {
@@ -43,26 +43,26 @@ module.exports = {
       const id = req.params.id
       const {title, description, author} = req.body
       const result = await bookModel.update({id, title, description, author})
-      res.json({
+      res.status(200).json({
         data: result,
         message: "update book success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
   destroy: async(req, res) => {
     try {
       const id = req.params.id
       const result = await bookModel.destroy(id)
-      res.json({
+      res.status(200).json({
         data: result,
         message: "delete book success"
       })
     } catch (error) {
       console.log(error)
-      res.json(error)
+      res.status(500).json(error)
     }
   },
 }
